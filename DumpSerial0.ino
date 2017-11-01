3fa3806af9821d749fc3f25ca9530bdc5594961e
liauihui <33207264+liauihui@users.noreply.github.com>
2017-11-01 10:36:08 +0800

    Add files via upload
    si iong "choose your file" zoxéx.

---
+++ DumpSerial0.ino
@@ -0,0 +1,66 @@
+#ifndef SS_SOFTWARE_SERIAL
+#define SS_SOFTWARE_SERIAL
+#include <SoftwareSerial.h>
+#endif
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("baud:9600");
+  Serial.println("CHR HEX BIN OCT DEC");
+}
+
+//--------------------------------------------------------
+void loop() {
+  
+  String str = "";
+
+  while ( Serial.available() > 0 ) {
+
+    int iByte = Serial.read();
+
+    if ( 0 == bitRead(iByte, 7) ) {
+      Serial.print(" ");
+    }
+
+    Serial.print("[");
+    Serial.print(char(iByte));
+    Serial.print("]");
+
+    Serial.print(" ");
+
+    Serial.print(iByte, HEX);
+    Serial.print(" ");
+
+    str = String(iByte, BIN);
+    while ( str.length() < 8 ) {
+      str = String("0") + str;
+    }
+    Serial.print(str);
+
+    Serial.print(" ");
+
+    str = String(iByte, OCT);
+    while ( str.length() < 3 ) {
+      str = String(" ") + str;
+    }
+    Serial.print(str);
+    Serial.print(" ");
+
+    str = String(iByte, DEC);
+    while ( str.length() < 3 ) {
+      str = String(" ") + str;
+    }
+    Serial.print(str);
+    Serial.print(" ");
+
+    
+    Serial.print("-----");
+   
+    Serial.println(""); // newLine
+
+    
+  }//WHILE
+
+  delay(1000);
+
+}//LOOP
